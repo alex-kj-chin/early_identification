2fcd64f6374cf0e22b57fc194c368dcfab183f78
Alex Chin <alexanderchin@college.harvard.edu>
2020-04-05 11:25:42 -0700

    finished the package

--- setup.py
+++ setup.py
@@ -4,19 +4,18 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="early-identification-alex-kj-chin", # Replace with your own username
+    name="early-identification-alex-kj-chin",
     version="0.0.1",
-    author="Example Author",
-    author_email="author@example.com",
-    description="A small example package",
+    author="Aelx Chin",
+    author_email="alexanderchin@college.harvard.edu",
+    description="A package to identify struggling students based on gradescope data",
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/alex-kj-chin/early_identification",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.7',
 )
